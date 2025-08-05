79af7586580e9b4972e4c635c9d1d01caaf81c7b
Jose Carlos Sotelo Vega <carlossotelovega@gmail.com>
2025-08-04 23:09:32 -0700

    Habilitar Scroll al hacer click

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const botonCarta = document.querySelector('.botonCarta');
+
+    botonCarta.addEventListener('click', () => {
+        document.body.style.overflow = 'auto';
+    });
+});
+
