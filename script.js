4ba8a7e6878f9481fd5c0e4ebec639c2e206c60d
Jose Carlos Sotelo Vega <carlossotelovega@gmail.com>
2025-08-12 14:51:00 -0700

    Contador, estilos main móvil

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// REGISTRAR EFECTO, ANIMACIÓN PARA DESAPARECER
 gsap.registerEffect({
     name: "fade",
     effect: (targets, config) => {
@@ -8,6 +9,7 @@ gsap.registerEffect({
     extendTimeline: true,
 });
 
+// REGISTRAR EFECTO, ANIMACIÓN PARA APARECER
 gsap.registerEffect({
     name: "fadeIn",
     effect: (targets, config) => {
@@ -17,27 +19,70 @@ gsap.registerEffect({
     extendTimeline: true,
 });
 
+// ANIMACIÓN PARA LA CARTA
 document.addEventListener('DOMContentLoaded', () => {
-    const botonCarta = document.querySelector('.botonCarta');
-    const imagenInicial = document.querySelector('.imagenInicial');
+    const botonCarta = document.querySelector('.carta-boton');
+    const imagenInicial = document.querySelector('.carta-imagen-inicial');
 
     botonCarta.addEventListener('click', () => {
         document.body.style.overflow = 'auto';
         gsap.effects.fade(botonCarta);
 
         if (screen.width > 601) {
-            gsap.to(".sobreDerecha", { x: 1000, duration: 2.5 });
-            gsap.to(".sobreIzquierda", { x: -1200, duration: 2.5 });
+            gsap.to(".carta-sobre-derecha", { x: 1000, duration: 2.5 });
+            gsap.to(".carta-sobre-izquierda", { x: -1200, duration: 2.5 });
         } else {
-            gsap.to(".sobreDerecha", { x: 300, duration: 1.5 });
-            gsap.to(".sobreIzquierda", { x: -400, duration: 1.5 });
+            gsap.to(".carta-sobre-derecha", { x: 300, duration: 1.5 });
+            gsap.to(".carta-sobre-izquierda", { x: -400, duration: 1.5 });
         }
 
         gsap.effects.fadeIn(imagenInicial);
 
         setTimeout(() => {
-            document.querySelector('main').style.display = 'block';
+            document.querySelector('main').style.display = 'flex';
         }, 500);
 
     });
 });
+
+
+// LOGICA DEL CONTADOR
+
+const fechaBoda = new Date(2025, 10, 29, 19, 0, 0).getTime();
+
+let actualizarContador = setInterval(() => {
+    let hoy = new Date().getTime();
+
+    // Calculo de los milisegundos entre la fecha de la boda y el dia actual
+    let distancia = fechaBoda - hoy;
+
+    // Calculo de dias, horas, minutos y segundos faltantes para la boda
+    let dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    let horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutos = Math.floor((distancia % (1000 * 60 * 60) / (1000 * 60)));
+    let segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+    if (dias.toString().length < 2) {
+        document.querySelector(".contador-dias").innerHTML = "0" + dias + ":";
+    } else {
+        document.querySelector(".contador-dias").innerHTML = dias + ":";
+    }
+
+    if (horas.toString().length < 2) {
+        document.querySelector(".contador-horas").innerHTML = "0" + horas + ":";
+    } else {
+        document.querySelector(".contador-horas").innerHTML = horas + ":";
+    }
+
+    if (minutos.toString().length < 2) {
+        document.querySelector(".contador-minutos").innerHTML = "0" + minutos + ":";
+    } else {
+        document.querySelector(".contador-minutos").innerHTML = minutos + ":";
+    }
+
+    if (segundos.toString().length < 2) {
+        document.querySelector(".contador-segundos").innerHTML = "0" + segundos;
+    } else {
+        document.querySelector(".contador-segundos").innerHTML = segundos;
+    }
+}, 1000);
