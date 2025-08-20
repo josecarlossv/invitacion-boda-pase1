4e5793e50744a38e799d57d2816918bf2f756571
Jose Carlos Sotelo Vega <carlossotelovega@gmail.com>
2025-08-20 14:23:53 -0700

    Animación boton de envio de formulario

--- script.js
+++ script.js
@@ -315,9 +315,14 @@ const form = document.forms['confirmacion-formulario'];
 const dialog = document.getElementById('confirmacion-dialog');
 const dialogError = document.getElementById('error-dialog');
 const recargar = document.querySelector('.recargar');
+const botonConfirmacion = document.querySelector('.boton-confirmacion')
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    botonConfirmacion.disabled = true;
+    botonConfirmacion.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"><animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></svg>';
+
+
     fetch(URLform, { method: 'POST', body: new FormData(form) })
         .then(response => dialog.showModal())
         .then(() => recargar.addEventListener('click', () => {
@@ -325,5 +330,6 @@ form.addEventListener('submit', e => {
         }))
         .catch(error => dialogError.showModal())
 })
+
 
 
